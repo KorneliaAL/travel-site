5db2f3e27110183f5c90f8f2bc1ab634a2b35158
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-24 12:49:47 +0100

    Implemented slideshow functionality

--- js/module/slideshow.js
+++ js/module/slideshow.js
@@ -3,6 +3,57 @@ export default function Slideshow() {
 
 	const buttonPrevious = document.querySelector('.destination__button-previous');
 	const buttonNext = document.querySelector('.destination__button-next');
-	const imageslides = document.querySelectorAll('.destination__slide');
+	const imageSlides = document.querySelectorAll('.destination__slide');
+	const slideDots = document.querySelectorAll('.destination__dots');
 
+	buttonPrevious.addEventListener('click', handleButtonPreviousClick);
+	buttonNext.addEventListener('click', handleButtonNextClick);
+
+	for (let index = 0; index < slideDots.length; index += 1) {
+		slideDots[index].addEventListener('click', event => {
+			handleSlideDotsClick(event, index);
+		})
+	}
+
+	
+	function handleButtonPreviousClick(event) {
+		decreaseIndex();
+		renderHTML();
+	}
+
+	function handleButtonNextClick(event) {
+		increaseIndex();
+		renderHTML();
+	}
+
+	function handleSlideDotsClick(event, index) {
+		slideIndex = index;
+		renderHTML();
+	}
+
+	function decreaseIndex() {
+		if (slideIndex === 0) {
+			slideIndex = imageSlides.length - 1;
+		}
+		slideIndex -= 1;
+	}
+
+	function increaseIndex() {
+		slideIndex += 1;
+		if (slideIndex > imageSlides.length - 1) {
+			slideIndex = 0;
+		}
+	}
+
+	function renderHTML() {
+		for (const slide of imageSlides) {
+			slide.classList.remove('destination__slide--visible');
+		}
+		imageSlides[slideIndex].classList.add('destination__slide--visible');
+
+		for (const dot of slideDots) {
+			dot.classList.remove('destination__dots--active');
+		}
+		slideDots[slideIndex].classList.add('destination__dots--active');
+	}
 }
