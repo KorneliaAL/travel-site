57d9a9668327eb174c4f61704ad64bfc9e339f50
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-28 14:31:00 +0100

    Optimized code

--- js/module/slideshow.js
+++ js/module/slideshow.js
@@ -1,89 +1,50 @@
-export default function Slideshow() {
-
+export default function Slideshow(slideshowContainer) {
+	console.log(slideshowContainer);
 
 	// Data
 	let slideIndex = 0;
-	let slideShow;
-	let slideDots;
+	// let slideShow;
+	let slideshowVisible = 'japan';
 
 	/**
 	 * @todo debug the dots, and decrease button. The dots sometimes skip one. The previous button dont show the last img, 
-	 * and the dots can only be pushed at japan. Need to export handleSlideDotsClick(); to destination.js?
+	 * and the dots can only be clicked at japan.
 	 */
 
 	// Variables
-	const buttonPrevious = document.querySelectorAll('.destination__button-previous');
-	const buttonNext = document.querySelectorAll('.destination__button-next');
-	const imageSlidesJapan = document.querySelectorAll('.destination__slide-japan');
-	const imageSlidesLaos = document.querySelectorAll('.destination__slide-laos');
-	const imageSlidesMaldives = document.querySelectorAll('.destination__slide-maldives');
-	const imageSlidesCambodia = document.querySelectorAll('.destination__slide-cambodia')
-	const slideDotsJapan = document.querySelectorAll('.destination__dots-japan');
-	const slideDotsLaos = document.querySelectorAll('.destination__dots-laos');
-	const slideDotsMaldives = document.querySelectorAll('.destination__dots-maldives');
-	const slideDotsCambodia = document.querySelectorAll('.destination__dots-cambodia');
+	const slideshowSlides = slideshowContainer.querySelectorAll('.destination__slide');
+	const slideDots = slideshowContainer.querySelectorAll('.destination__dots');
+	const buttonPrevious = slideshowContainer.querySelectorAll('.destination__button-previous');
+	const buttonNext = slideshowContainer.querySelectorAll('.destination__button-next');
+	console.log(slideshowContainer);
 
-	getCurrentSlideshow();
 	// Event listeners
-	buttonNext.forEach(button => {
-		button.addEventListener('click', handleButtonNextClick);
-	})
+	if (slideshowContainer) {
+		buttonNext.forEach(button => {
+			button.addEventListener('click', handleButtonNextClick);
+		});
 
-	buttonPrevious.forEach(button => {
-		button.addEventListener('click', handleButtonPreviousClick);
-	})
+		buttonPrevious.forEach(button => {
+			button.addEventListener('click', handleButtonPreviousClick);
+		});
 
-	slideDots.forEach((dot) => {
-		dot.addEventListener('click', handleSlideDotsClick)
+		slideDots.forEach((dot) => {
+			dot.addEventListener('click', handleSlideDotsClick)
+		});
+	}
 
-	})
 	// Event handlers
 	function handleButtonPreviousClick() {
-		getCurrentSlideshow();
 		decreaseIndex();
 		renderHTML();
 	}
 
 	function handleButtonNextClick() {
-		getCurrentSlideshow();
 		increaseIndex();
 		renderHTML();
 	}
 
 	// Methods
-	/**
-	 * Shows different slideshows, depending on which country selectet. Gets current country
-	 * by importing data from destination.js. 
-	 * @example clicked(japan) returns imageSlidesJapan and slideDotsJapan
-	 */
-	function getCurrentSlideshow() {
-		let currentCountry = localStorage.getItem('Country');
-		switch (currentCountry) {
-			case 'japan':
-				slideShow = imageSlidesJapan;
-				slideDots = slideDotsJapan;
-				break;
-
-			case 'laos':
-				slideShow = imageSlidesLaos;
-				slideDots = slideDotsLaos;
-				break;
-
-			case 'maldives':
-				slideShow = imageSlidesMaldives;
-				slideDots = slideDotsMaldives;
-				break;
-
-			case 'cambodia':
-				slideShow = imageSlidesCambodia;
-				slideDots = slideDotsCambodia;
-		}
-
-	}
-	/**
-	 * Changes the slideIndex to data-index and calls renderHTML
-	 * @param {*} event 
-	 */
 	function handleSlideDotsClick(event) {
 
 		slideIndex = event.currentTarget.dataset.index;
@@ -92,30 +53,31 @@ export default function Slideshow() {
 
 	function decreaseIndex() {
 		if (slideIndex === 0) {
-			slideIndex = slideShow.length - 1;
+			slideIndex = slideshowSlides.length - 1;
 		} else {
 			slideIndex -= 1;
 		}
-
 	}
 
 	function increaseIndex() {
 		slideIndex += 1;
-		if (slideIndex > slideShow.length - 1) {
+		if (slideIndex > slideshowSlides.length - 1) {
 			slideIndex = 0;
 		}
 	}
 
 	// Render
 	function renderHTML() {
-		for (const slide of slideShow) {
+		for (const slide of slideshowSlides) {
 			slide.classList.remove('destination__slide--visible');
 		}
-		slideShow[slideIndex].classList.add('destination__slide--visible');
+		slideshowSlides[slideIndex].classList.add('destination__slide--visible');
 
 		for (const dot of slideDots) {
 			dot.classList.remove('destination__dots--active');
 		}
 		slideDots[slideIndex].classList.add('destination__dots--active');
 	}
+
+	renderHTML();
 }
