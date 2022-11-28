686098318e0ddc8c23936b6d311db6bc5d6cfee6
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-28 18:03:45 +0100

    Debugging

--- js/module/header.js
+++ js/module/header.js
@@ -3,7 +3,6 @@ export default function Header() {
 
 	const headerNavigation = document.querySelector('.header__navigation');
 	const headerButtonMenu = document.querySelector('.header__button-menu');
-	const navigationButtonText = document.querySelector('.visually-hidden');
 
 	headerButtonMenu.addEventListener('click', handleHeaderButtonMenuClick);
 
@@ -20,12 +19,12 @@ export default function Header() {
 		if (navigationVisible === true) {
 			headerNavigation.classList.add('header__navigation--visible');
 			headerButtonMenu.style.backgroundImage = 'url(../../assets/icons/exit.svg)';
-			navigationButtonText.setAttribute('aria-label', 'Lukk');
+			headerButtonMenu.setAttribute('aria-label', 'Lukk');
 
 		} else {
 			headerNavigation.classList.remove('header__navigation--visible');
 			headerButtonMenu.style.backgroundImage = 'url(../../assets/icons/menu.svg)';
-			navigationButtonText.setAttribute('aria-label', 'Meny');
+			headerButtonMenu.setAttribute('aria-label', 'Meny');
 		}
 	}
 }
