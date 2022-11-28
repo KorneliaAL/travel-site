b9d86b087c17f226281d8963263b8fd8bc8a4b42
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-28 10:32:32 +0100

    Removed passing data through models

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 import Header from './module/header.js';
 import Slideshow from './module/slideshow.js';
-import * as Destination from './module/destination.js';
+import Destination from './module/destination.js';
 import Packinglist from './module/packinglist.js'
 
+const destination = document.querySelector('.destination');
+const packinglist = document.querySelector('.packinglist');
+
 Header();
-Destination.Destination();
-Slideshow(Destination.currentCountry);
-Packinglist();
+
+if (destination) {
+	Slideshow();
+	Destination();
+}
+
+if (packinglist) {
+	Packinglist();
+}
+
