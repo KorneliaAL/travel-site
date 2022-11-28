d52f2f1c57f0024d9215dab11086ec27c81aa164
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-28 10:16:26 +0100

    Imported packinglist.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 import Header from './module/header.js';
 import Slideshow from './module/slideshow.js';
 import * as Destination from './module/destination.js';
+import Packinglist from './module/packinglist.js'
 
 Header();
 Destination.Destination();
 Slideshow(Destination.currentCountry);
+Packinglist();
