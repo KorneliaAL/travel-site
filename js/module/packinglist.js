16d8ea50cf4f63adc9918120d2ca4eea4b5f7ce2
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-29 15:34:28 +0100

    Added event listener

--- js/module/packinglist.js
+++ js/module/packinglist.js
@@ -2,4 +2,8 @@ export default function Packinglist() {
 	const packinglistInput = document.querySelector('packinglist__input');
 	const packinglistAddButton = document.querySelector('packinglist__add-button');
 	const packinglistContainer = document.querySelector('.packinglist__container');
+
+	packinglistAddButton.addEventListener('click', handlePackinglistAddButton);
 }
+
+
