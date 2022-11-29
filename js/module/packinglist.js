f0a743dc5f126b517ca2ea1fd3d5caab11396e3c
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-29 15:31:06 +0100

    Added query selectors

---
+++ js/module/packinglist.js
@@ -0,0 +1,5 @@
+export default function Packinglist() {
+	const packinglistInput = document.querySelector('packinglist__input');
+	const packinglistAddButton = document.querySelector('packinglist__add-button');
+	const packinglistContainer = document.querySelector('.packinglist__container');
+}
