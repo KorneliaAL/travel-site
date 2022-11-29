8ff7ceea3bf987beeaa4ddd9fb9c3801efa7309b
Kornelia Arnet Lyberg <104372863+KorneliaAL@users.noreply.github.com>
2022-11-29 15:26:01 +0100

    Imported module packinglist.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
 import Header from './module/header.js';
 import Slideshow from './module/slideshow.js';
 import Destination from './module/destination.js';
-// import Packinglist from './module/packinglist.js'
-
-const destination = document.querySelector('.destination');
-// const packinglist = document.querySelector('.packinglist');
+import Packinglist from './module/packinglist.js'
 
 Header();
 Destination();
 for (const slideshowContainer of document.querySelectorAll('.destination__slideshow')) {
 	Slideshow(slideshowContainer);
 }
+Packinglist();
 
 
 
